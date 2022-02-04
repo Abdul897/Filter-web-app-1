520c0873d84a05510e7df12e4b700083a6d74aeb
Abdul897 <85251452+Abdul897@users.noreply.github.com>
2022-02-04 21:03:15 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function take_snapshot(){
+    save('Filterweb.png');
+}
+function draw() {
+    image(video, 0, 0,  300, 300);
+    video.size(300,300);
+    video.hide();
+}
+function preload() {
+}
+
+function setup() {
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+}
